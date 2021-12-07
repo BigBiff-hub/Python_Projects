4d5cf5a8ae289f3a59144d5508157144852e01bb
BigBiff-hub <77340730+BigBiff-hub@users.noreply.github.com>
2021-12-07 00:50:15 +0000

    prints out passwords

---
+++ passwordgennie.py
@@ -0,0 +1,19 @@
+import random
+
+print("Password Gennie")
+
+PASS_AMOUNT = input("How many passwords would you like?")
+PASS_AMOUNT = int(PASS_AMOUNT)
+CHAR_AMOUNT = input("How long would you like your Password? ")
+CHAR_AMOUNT = int(CHAR_AMOUNT)
+
+chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+print("Here are your passwords!!")
+
+for p in range(0, PASS_AMOUNT):
+    pwd = ''
+    for c in range(0, CHAR_AMOUNT):
+        pwd += random.choice(chars)
+    print(pwd)
+
+
