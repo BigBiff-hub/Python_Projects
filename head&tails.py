75b991de6c936b3a7509e7419aac094a0a61e58e
BigBiff-hub <77340730+BigBiff-hub@users.noreply.github.com>
2021-12-08 12:21:12 +0000

    chance of streak = 80%

--- head&tails.py
+++ head&tails.py
@@ -2,5 +2,19 @@ import random
 
 number_of_streaks = 0
 
-for exp_number in range(0, 10000):
-     pass
+for experiment_number in range(10000):
+
+    # Code that creates a list of 100 'heads' or 'tails' values
+    coin_list = ''
+    for i in range(100):
+        if random.randint(0, 1) == 0:
+            coin_list += 'H'  # heads
+        else:
+            coin_list += 'T'  # tails
+
+    # Code that checks if there is a streak of 6 heads or tails in a row
+    if 'HHHHHH' in coin_list or 'TTTTTT' in coin_list:
+        number_of_streaks += 1
+
+print('Chance of streak: %s%%' % (number_of_streaks / 100))
+
