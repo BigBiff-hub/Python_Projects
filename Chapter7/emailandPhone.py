a73700d7215f64e0978285c1423b85db7348b807
BigBiff-hub <77340730+BigBiff-hub@users.noreply.github.com>
2022-08-18 02:10:26 +0100

    add email regex

--- Chapter7/emailandPhone.py
+++ Chapter7/emailandPhone.py
@@ -2,9 +2,16 @@
 import  pyperclip, re
 
 phoneRegex  = re.compile(r'''(
-                        ( \d{3}|\(\d{3}\))?
+                        ( \d{3}|\(\d{3}\))? #area code
                         (\s|-|\.)?
-                        (\d{3})
+                        (\d{3}) 
                         (\s|-|\.)
                         (\d{4}) 
-                        (\s*(ext|x|ext.)\s*(\d{2,5}))?)''', re.VERBOSE)
+                        (\s*(ext|x|ext.)\s*(\d{2,5}))?)''', re.VERBOSE) #extention
+
+emailRegex = re.compile(r'''
+                        ([a-zA-Z0-9._%+-]+
+                        @
+                        [a-zA-Z0-9._%+-]+
+                        (\.[a-zA-Z]{2,4})
+                        )''', re.VERBOSE)
